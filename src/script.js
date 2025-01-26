b7439dbb66cbf8c08dcbfbae8d59842a86254c1d
ajith-1 <ajithm0216@gmail.com>
2025-01-26 20:16:34 +0530

    Variables and Data types

--- src/script.js
+++ src/script.js
@@ -1,37 +1,185 @@
 
+/* 1.Javascript is a high-level programming language primarily used to create intractive and dynamic web content.
+2.it allows developers to build responsive user interface, manipulate web pages in real time handle events, and communicate with servers.*/
 
 
-// Datatypes
+// JavaScript Topics
+
+// 1. Variables & Values (variables are named storage for data values. they allow you to store and manipulate data with your program.)
+
+// types of variables --- let, const and var
+
+// Declare and initialize a variable
+let x = 10;
+console.log(x); // result -- 10
+
+// Declare a variable
+let y;
+console.log(y); // result -- undefined 
+
+// Initialize a variable
+y = 20;
+console.log(y); // result -- 20
+
+// Naming conversation 
+let userName = 'John'; // Correct camelCase convertion
+let user_name = 'Max'; // Underscore is not recommended but still valid
+
+// Incorrect variable names
+// let 5date=5; Invalid variable names connot start with a number
+// let John&Jane='Friends'; Invalid variable names connot contain with a special character 
+
+// Reserved keywords
+// let new = 'date';
+// let function = 'date';
+
+// Symbols in variable names
+// let #function = 'date';
+// let @function = 'date';
+let $function = 'date';
+
+// Constants
+
+let sun = 'Sun raise in the East';
+console.log(sun); // result -- "Sun raise in the East" 
+
+sun = 'Sun raise in the West';
+console.log(sun); // result -- "Sun raise in the West" // Reassigned value changed in let keyword. 
+
+// const must decleare and initialize 
+
+const PI = 3.1415;
+console.log(PI); // result -- 3.1415
+
+// PI='test';
+// console.log(PI); // result -- test // Reassigned value cannot be changed in const keyword. 
+
+// var (var is a scope level variable)
+
+var job = 'Developer';
+console.log(job); // result -- Developer
+
+job = 'Project Manager';
+console.log(job); // result -- Project Manager 
+
+var a = 1;
+console.log(a);
+var a = 5; // Redeclaration is allowed 
+console.log(a);
+
+
+let b = 10;
+// let b=20; // Redeclaration is not allowed 
+b = 20; // Reassignment is allowed
+console.log(b);
+
+const c = 1;
+console.log(c);
+// c = 3; // Reassignment is allowed
+
+/* Scope of variable will on functions & loops
+var : Function scoped.
+let : Block scoped.
+const : Block scoped.
+*/
+
+// ---------------------DATA TYPES-----------------------
+
+// Datatypes (Data types are rhe different kinds of information thst ypu can store in variables.Each type of information has a specific purpose and behavior.)
+/* 1. Permitive Data Types
+        Number, String, Boolean, Undefined, Null, Symbol, Big Int.
+   2. Non-Permitive Data Types
+        Object, Array, Function, Data.
+*/
+
 // Javascript is Dynamic Typing
 // Primitive and Object Type
 
 //Primitive Data Types
 //1.Number - Represents both integer and floating-point numbers.
+let age = 25;
+console.log(age); // result -- 25
+console.log(typeof age); // result -- number 
+
+age = '25';
+console.log(age); // result -- 25
+console.log(typeof age); // result -- string
+
+
+let price = 15.45;
+console.log(price); // result -- 15.45
+console.log(typeof price); // result -- number
+
 
 //2.String - Represents a sequence of characters.
+let greeting = 'Hello world';
+console.log(greeting); // result -- Hello world
+console.log(typeof greeting); // result -- string 
 
 //3.Boolean - Represents a logical entity and can have two values: true or false.
+let isActive = true;
+console.log(isActive); // result -- true
+isActive = false;
+console.log(isActive); // result -- false
 
 //4.Undefined - A variable that has been declared but not assigned a value.
+let uninitialized;
+console.log(uninitialized); // result -- undefined
 
 //5.Null - Represents the intentional absence of any object value.
+let box = null;
+console.log(box); // result -- null
 
 //6.Symbol - Represents a unique and immutable value, often used as object property keys.
+let unique = Symbol('key');
+console.log(unique); // result -- Symbol('key')
 
 //7.BigInt - Represents whole numbers larger than 2^53 - 1 (the largest number JavaScript can reliably represent with the Number type).
+let largeNumber = BigInt(979696716432788754);
+let largerNumber = 979696716432788754n;
+console.log(largeNumber);  // result -- 979696716432788754n
+console.log(largerNumber); // result -- 979696716432788754n
+
 
 //Non-Primitive Data Types
 //1.Object
 /*Represents a collection of properties, each consisting of a key (usually a string) 
 and a value (which can be any data type, including another object).*/
 
+let person = {
+    name:'John',
+    age:25,
+    isEmployed:true,
+};
+
+console.log(person); // result -- full object
+console.log(person.name); // result -- John
+console.log(person.age); // result -- 25
+console.log(person.isEmployed); // result -- true
+
+let emptyObject = {};
+console.log(emptyObject); // result -- Object( )
+
 //2.Array
 //A special type of object used for storing ordered collections of values.
+let numbers = [2,4,3,6,8,5];
+console.log(numbers); // result -- Array(6) [2,4,3,6,8,5]
+
+let mixedData = [2,'two',true,null];
+console.log(numbers); // result -- Array(4) [2,'two',true,null]
 
 //3.Function
 //A special type of object that is callable and can perform an action.
+function greeting(){
+    console.log('Hello guys');
+}
+
+greeting();  // calling function // result -- Hello guys
 
 //4.Date - A built-in object for handling dates and times.
+
+let now = new Date();
+console.log(now); // result -- current date
 
 // ------------------------------------------------------------------------------------------
 
@@ -197,7 +345,7 @@ console.log('Truthy and Falsy Values');
 // Calling the function
 
 // Function Parameters and Arguments
-/*Functions can take parameters, which act as placeholders for the values 
+/*Functions can take parameters, which act as placeholders for the values
 that will be passed to the function.The passing value is called an argument.*/
 
 // Default Parameter
@@ -239,14 +387,14 @@ const: Block scoped.
 // ------------------------------------------------------
 
 // Loops
-/* 
+/*
 In programming, loops are used to execute a block of code repeatedly until
 a specific condition is met or for a specified number of times.
 They provide a way to perform repetitive tasks efficiently without writing
 the same code multiple times.
 */
 
-/* 
+/*
 Types of Loops
 1. For Loop
 2. While Loop
@@ -266,7 +414,7 @@ Types of Loops
 // ------------------------------------------------------
 
 /*
-An array is a data structure that can hold multiple values at once. 
+An array is a data structure that can hold multiple values at once.
 These values can be of any type, including numbers, strings, objects, or even other arrays.
 Arrays in JavaScript are zero-indexed, meaning the first element is at index 0.
 */
@@ -278,20 +426,20 @@ Arrays in JavaScript are zero-indexed, meaning the first element is at index 0.
 // Change Value in Array
 
 /*
-Using for loop to print array with hard-coded condition 
+Using for loop to print array with hard-coded condition
 it will create issue if condition is like i < 5
 */
 
-/* 
+/*
 To avoid hard-coded condition switch to array methods
 */
 // We can use array length
 
 // More Array Methods
 
-/* 
+/*
 Add element to array
-Adds one or more elements to the end of an array 
+Adds one or more elements to the end of an array
 and returns the new length of the array.
 */
 
